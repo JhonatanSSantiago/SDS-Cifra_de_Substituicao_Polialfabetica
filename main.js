78b76219ba1b0847eea71547a0138a1183bb976f
Jhonatan Santiago <jhonssantiago@gmail.com>
2022-04-17 14:25:20 -0300

    Finalizado, sem espaço

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const espaco = ' ';
 
 document.getElementById("btn_cifrar").addEventListener("click",
     (evento)=>{
@@ -11,94 +10,96 @@ document.getElementById("btn_cifrar").addEventListener("click",
 function cifrarMsg(){
     var msg = document.getElementById("msg").value.toUpperCase();
     var chave = document.getElementById("chave").value.toUpperCase();
+    
     var tam_chave = chave.length;
     var msg_chave = "";
-    var msg_cifrada = "";
-    
+    var msg_cifrada = ""; 
     console.log("msg: "+msg+" |chave: "+chave+" |tam da chave: "+tam_chave);
-
-    for(var j = 0; j<msg.length; j++){ 
-        
-        msg_chave = msg.slice(0,tam_chave);
-        msg=msg.slice(tam_chave,msg.length);
-        console.log("msg-chave: "+msg_chave+" |Msg resto: "+msg);
-        
-        for(var i = 0; i<msg_chave.length;i++){
-            var letra_msg = msg_chave[i];
-            var letra_chave = chave[i];
-             console.log("letra na msg: "+letra_msg+" |letra na chave: "+letra_chave);
-    
-            var letra_alf_msg = alf.lastIndexOf(letra_msg);
-            var letra_alf_cha = alf.lastIndexOf(letra_chave);
-            console.log("letra da msg no alf: "+letra_alf_msg+" |letra da chave no alf: "+letra_alf_cha);
-    
-            var soma = letra_alf_cha+letra_alf_msg;
-            console.log("Soma: "+soma);
-       
-    
-            if ( soma > 25)  {
-                var resto = soma - 25;
-                msg_cifrada = msg_cifrada + alf[resto - 1];     
-            } else{            
-                msg_cifrada = msg_cifrada + alf[soma];    
-            }   
+    if(tam_chave<3){
+        alert("digita uma chave com pelo menos 3 caracteres");
+    }else{       
+        for(var j = 0; j<=msg.length; j++){ 
+            msg_chave = msg.slice(0,tam_chave);
+            msg=msg.slice(tam_chave);
+            console.log("msg-chave: "+msg_chave+" |Msg resto: "+msg);
             
-        } //for i
-
-          /*  if(msg.length<tam_chave){ 
-                var letra_msg = msg[j];
-                var letra_chave = chave[j];
-                console.log("MSG < CHAVE| letra na msg: "+letra_msg+" |letra na chave: "+letra_chave);
-
+            for(var i = 0; i<msg_chave.length;i++){
+                var letra_msg = msg_chave[i];
+                var letra_chave = chave[i];
+                console.log("letra na msg: "+letra_msg+" |letra na chave: "+letra_chave);
+        
                 var letra_alf_msg = alf.lastIndexOf(letra_msg);
                 var letra_alf_cha = alf.lastIndexOf(letra_chave);
-                console.log("MSG < CHAVE| letra da msg no alf: "+letra_alf_msg+" |letra da chave no alf: "+letra_alf_cha);
-
+                console.log("letra da msg no alf: "+letra_alf_msg+" |letra da chave no alf: "+letra_alf_cha);
+        
                 var soma = letra_alf_cha+letra_alf_msg;
-                console.log("MSG < CHAVE|  SOMA: "+soma);
-    
+                console.log("Soma: "+soma);
+            
                 if ( soma > 25)  {
                     var resto = soma - 25;
-                    console.log("MSG < CHAVE| RESTO: "+resto);
                     msg_cifrada = msg_cifrada + alf[resto - 1];     
                 } else{            
                     msg_cifrada = msg_cifrada + alf[soma];    
-                }
-            } //if msg < chave  */
-       
-    } //for j
+                }   
+                
+            } 
+        
+        } //for j
+        resposta(msg_cifrada)
+    }     
     
-    resposta(msg_cifrada)
+    
 }
 
 
 document.getElementById("btn_descifrar").addEventListener("click",
 (evento)=>{
     evento.preventDefault();
-
     descifrarMsg();
 
 });
 
 function descifrarMsg(){
-    var msg = document.getElementById("msg").value.toLowerCase();
-    var chave = document.getElementById("chave").value.length;
-    var msg_cifrada = '';
-    for (var i = 0; i < msg.length; i++ ){ 
-        if (msg[i] == espaco){
-            msg_cifrada = msg_cifrada + espaco; 
-        }else{
-            var letra_msg = msg[i];
-            var letra_alf = letras_alf.lastIndexOf(letra_msg); 
-            if ( (letra_alf - chave) < 0)  {
-                var resto = (parseInt(letra_alf) - chave) + 24;
-                msg_cifrada = msg_cifrada +  letras_alf[2 + resto];    
-            }else{
-                msg_cifrada = msg_cifrada +  letras_alf[parseInt(letra_alf) - chave];    
-            }
-        }   
-    } 
-    resposta(msg_cifrada);
+    var msg = document.getElementById("msg").value.toUpperCase();
+    var chave = document.getElementById("chave").value.toUpperCase();
+    var tam_chave = chave.length;
+    var msg_chave = "";
+    var msg_cifrada = "";   
+    console.log("msg: "+msg+" |chave: "+chave+" |tam da chave: "+tam_chave);
+    if(tam_chave<3){
+        alert("digita uma chave com pelo menos 3 caracteres");
+    }else{     
+        for(var j = 0; j<=msg.length; j++){ 
+            
+            msg_chave = msg.slice(0,tam_chave);
+            msg=msg.slice(tam_chave);
+            console.log("msg-chave: "+msg_chave+" |Msg resto: "+msg);
+            
+            for(var i = 0; i<msg_chave.length;i++){
+                var letra_msg = msg_chave[i];
+                var letra_chave = chave[i];
+                console.log("letra na msg: "+letra_msg+" |letra na chave: "+letra_chave);
+        
+                var letra_alf_msg = alf.lastIndexOf(letra_msg);
+                var letra_alf_cha = alf.lastIndexOf(letra_chave);
+                console.log("letra da msg no alf: "+letra_alf_msg+" |letra da chave no alf: "+letra_alf_cha);
+        
+                var sub = letra_alf_msg-letra_alf_cha;
+                console.log("Sub: "+sub);
+        
+        
+                if ( sub < 0)  {
+                    var resto = sub + 24;
+                    msg_cifrada = msg_cifrada + alf[2 + resto];     
+                } else{            
+                    msg_cifrada = msg_cifrada + alf[sub];    
+                }   
+                
+            } 
+        
+        } //for j
+        resposta(msg_cifrada);
+    }
 }
 
 function resposta(msg_cifrada){
